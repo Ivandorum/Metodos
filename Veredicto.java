b4db0f6ec8b91fa584e5cfb08834b3e7aa2ee226
Iván <ivanagullosaez24@gmail.com>
2022-11-16 10:33:27 +0100

    Creados ficheros.java

---
+++ Veredicto.java
@@ -0,0 +1,12 @@
+private class Veredicto {
+   public void v(String num1, String num2) {
+           if (num1.equals(num2)) {
+               System.out.println("Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.");
+           } else if (Integer.valueOf(num1) > Integer.valueOf(num2)) {
+               System.out.println("Poco tiempo de estudio. Debes dedicar más tiempo.") ;
+           } else {
+               System.out.println("Ideal. Trabajas los contenidos en casa.");
+           }
+
+   }
+}
---
+++ proyectofuturo.java
@@ -0,0 +1,10 @@
+public class proyectofuturo {
+   public static void main (String [] args) {
+       String num1 = (args[1]);
+       String num2 = (args[2]);
+
+       Veredicto veredicto = new Veredicto();
+       System.out.println("Estudiante " + args[0] + " : ");
+       veredicto.v(num1,num2);
+   }
+}
